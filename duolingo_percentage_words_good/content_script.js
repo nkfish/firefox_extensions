713748594ea7390797036e4a4fc405600a4f6b18
Nathaniel Fish <nkfish@gmail.com>
2018-02-10 23:15:45 -0800

    try to make extension more reliable using MutationObserver and setTimeout

--- duolingo_percentage_words_good/content_script.js
+++ duolingo_percentage_words_good/content_script.js
@@ -1,28 +1,67 @@
-console.log("loading duolingo_percentage_words_good extension...");
+console.log("extension: loaded...");
 
-let strengthOne = document.querySelectorAll(".strength-1").length - 1;
-let strengthTwo = document.querySelectorAll(".strength-2").length - 1;
-let strengthThree = document.querySelectorAll(".strength-3").length - 1;
-let strengthFour = document.querySelectorAll(".strength-4").length - 1;
+var timeout = null;
 
-
-let percentGood = 100 * (strengthFour + strengthThree )
-                    / (strengthFour + strengthThree + strengthTwo + strengthOne);
-
-percentGood = percentGood.toFixed(3);
-
-let span = document.getElementById("duolingo_percentage_words_good_id");
-if (span !== null)
+function callback()
 {
-    span.parentElement.removeChild(span);
-    console.log("removing old extension span element...");
+    console.log("extension: in callback...");
+
+    if (timeout !== null)
+    {
+        console.log("extension: reset timeout");
+        clearTimeout(timeout);
+        timeout = null;
+    }
+
+    if (document.readyState !== "complete")
+    {
+        console.log("extension: readyState is " + document.readyState);
+        timeout = setTimeout(callback, 2000);
+        return;
+    }
+    else
+    {
+        observer.disconnect();
+    }
+
+    let strengthOne = document.querySelectorAll(".strength-1").length - 1;
+    let strengthTwo = document.querySelectorAll(".strength-2").length - 1;
+    let strengthThree = document.querySelectorAll(".strength-3").length - 1;
+    let strengthFour = document.querySelectorAll(".strength-4").length - 1;
+
+
+    let percentGood = 100 * (strengthFour + strengthThree )
+                        / (strengthFour + strengthThree + strengthTwo + strengthOne);
+
+    percentGood = percentGood.toFixed(3);
+
+    let span = document.getElementById("duolingo_percentage_words_good_id");
+    if (span !== null)
+    {
+        console.log("extension: span already present...");
+        return;
+    }
+
+    span = document.createElement("span");
+    span.id = "duolingo_percentage_words_good_id";
+
+    let text = document.createTextNode(" (" + percentGood + "% good...)");
+    span.appendChild(text);
+
+    let wordCount = document.getElementById("word-count");
+    wordCount.parentElement.appendChild(span);
 }
 
-span = document.createElement("span");
-span.id = "duolingo_percentage_words_good_id";
+var targetNode = document.getElementById("app");
+var config = {childList: true, subtree: true};
+var observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
 
-let text = document.createTextNode(" (" + percentGood + "% good...)");
-span.appendChild(text);
+window.addEventListener("load", function()
+{
+    console.log("extension: in window load event listener...");
+    callback();
+})
 
-let wordCount = document.getElementById("word-count");
-wordCount.parentElement.appendChild(span);
+callback();
+
